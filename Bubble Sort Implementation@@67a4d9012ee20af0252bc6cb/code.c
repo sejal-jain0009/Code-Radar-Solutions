76e3ab712d33fd6190f57a2039a6677dc4633df9
Sejal <sejaljain0009@gmail.com>
2025-03-21 17:08:54 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -28,7 +28,6 @@ void printArray(int arr[], int n) {
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-
     printf("Original array: ");
     printArray(arr, n);
 
