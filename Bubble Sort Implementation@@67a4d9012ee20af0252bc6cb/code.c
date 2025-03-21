b23bd1caadc332c0e0382afe809a321cb8826d02
Sejal <sejaljain0009@gmail.com>
2025-03-21 16:58:34 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,48 +1,41 @@
-// #include<stdio.h>
-// void bubbleSort(int arr[],int n);
-// void printArray(int arr[],int n);
-// int main()
-// {
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     bubbleSort(arr,n);
-//     printArray(arr,n);
-//     return 0;
-// }
+#include <stdio.h>
 
-// void bubbleSort(int arr[], int n) {
-//     int i, j, temp;
-//     for (i = 0; i < n - 1; i++) {
-//         for (j = 0; j < n - i - 1; j++) {
-//             if (arr[j] > arr[j + 1]) { 
-//                 temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     }
-// }
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        // Last i elements are already sorted
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) { 
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
-// void printArray(int arr[], int n) {
-//     for (int i = 0; i < n; i++) {
-//         printf("%d ", arr[i]);
-//     }
-//     printf("\n");
-// }
-
-
-
-
-
-
-
+// Function to print an array
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+// Main function
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array in ascending order: ");
+    printArray(arr, n);
+
+    return 0;
 }
