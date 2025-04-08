d7c0d43c6ae662aa17b6127fa237bda839c4a480
Sejal <sejaljain0009@gmail.com>
2025-04-08 09:44:51 +0530

    Solution to Calculate Total and Average Marks of Students

---
+++ Calculate Total and Average Marks of Students@@67877d61dc107486ad5c2746/code.c
@@ -0,0 +1,41 @@
+// Your code here...
+#include <stdio.h>
+
+// Define the structure
+struct student {
+    int roll_no;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    int N;
+    float total = 0.0, average;
+
+    scanf("%d", &N);
+
+    struct student students[N];
+
+    // Input student data and compute total marks
+    for (int i = 0; i < N; i++) {
+       
+        scanf("%d", &students[i].roll_no);
+
+        
+        scanf(" %s", students[i].name);  // assumes name has no spaces
+
+        
+        scanf("%f", &students[i].marks);
+
+        total += students[i].marks;
+    }
+
+    // Calculate average
+    average = total / N;
+
+    // Display results
+    printf("Total Marks: %.2f\n", total);
+    printf("Average Marks : %.2f\n", average);
+
+    return 0;
+}
