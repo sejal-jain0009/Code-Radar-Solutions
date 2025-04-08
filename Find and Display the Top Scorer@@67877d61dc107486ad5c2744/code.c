686d84d55011f24e799db5663cf9e6b995d836bc
Sejal <sejaljain0009@gmail.com>
2025-04-08 09:29:43 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -38,7 +38,7 @@ int main() {
 
     // Display the top student details
     
-    printf(" Top Scorer: Roll Number: %d,", top_student.roll_no);
+    printf(" Top Scorer:  Roll Number: %d,", top_student.roll_no);
     printf(" Name: %s,", top_student.name);
     printf(" Marks: %.2f\n", top_student.marks);
 
